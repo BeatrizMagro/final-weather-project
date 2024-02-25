ff8a5efbc481e4ecf581370282287d2404081c54
BeatrizMagro <beatrizmagro1@gmail.com>
2024-02-25 09:45:19 +0000

    app.js updates

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ export default function App() {
           </a>{" "}
           and{" "}
           <a
-            href="https://suspicious-beaver-111c4d.netlify.com/"
+            href="https://regal-rolypoly-b0e507.netlify.app"
             target="_blank"
             rel="noopener noreferrer"
           >
